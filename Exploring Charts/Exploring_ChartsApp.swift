dd1e1569aa757aa7dca0ab25de4c82c76d16d59d
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-05-01 11:19:16 +0200

    Initial Commit

---
+++ Exploring Charts/Exploring_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exploring_ChartsApp.swift
+//  Exploring Charts
+//
+//  Created by Manyuchi, Carrington C on 2025/05/01.
+//
+
+import SwiftUI
+
+@main
+struct Exploring_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
